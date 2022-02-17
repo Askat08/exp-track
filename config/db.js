471b3c8b5b3572f900c9e82f928fbb182258a3c3
askat <askatbakyev@gmail.com>
2022-02-16 20:46:25 -0500

    update packages. add instruction to readme

--- config/db.js
+++ config/db.js
@@ -4,8 +4,7 @@ const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
 
     console.log(
